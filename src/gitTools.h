3fd399c65ae1987c1df9f9c6bd92705b3dd71fe6
Jacob Causon <jc29g12@soton.ac.uk>
2014-04-13 12:43:10 +0100

    fix/Removed last commit as it seems like a pull is now required

--- src/gitTools.h
+++ src/gitTools.h
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-#define GIT_LAST_HASH "git log origin/master --pretty=format:'%H' -n 1"
+#define GIT_LAST_HASH "git log --pretty=format:'%H' -n 1"
 #define GIT_PULL "git pull origin master 1> /dev/null 2> /dev/null"
 #define GIT_INIT "git init 1> /dev/null 2> /dev/null"
 #define GIT_ADD_REMOTE(URL) ("git remote add origin " + URL + " 1> /dev/null 2> /dev/null")
